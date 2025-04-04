a380152456f11e55cc3c2fd453b108d686fa7bde
mahmoud <mahkhairy@hotmail.com>
2025-04-03 19:37:56 -0500

    test

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib  # or pickle
 import pandas as pd
 
-#load term mmmm
+#load term mmmm hey ss
 # Load model and vectorizer
 model = joblib.load("model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
